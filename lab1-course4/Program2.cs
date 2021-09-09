7733e8b339980a30947a8287fd8e19cf3c64cdf1
Bohdan_Novickyi <61537968+psy-ger@users.noreply.github.com>
2021-09-09 13:04:38 +0300

    backup

--- lab1-course4/Program2.cs
+++ lab1-course4/Program2.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Program;
 
 namespace lab1_course4
 {
     class Program2
-    {
+	{
 
-    }
+    if (number > 1000)
+			{
+				thousands = (number / 1000) * 10;
+			}
+	int hundreds = (number % 1000) / 100;
+	int sum = thousands + hundreds;
 }
+
