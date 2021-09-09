b0d3b91351d8318cf77174d9aad1a4313f28be07
Bohdan_Novickyi <61537968+psy-ger@users.noreply.github.com>
2021-09-09 12:21:01 +0300

    add file-2

--- lab1-course4/Program.cs
+++ lab1-course4/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
+public class Program
+	{
+		public static void Main()
+		{
+			int thousands = 1;
+			int number = Convert.ToInt32(Console.ReadLine());
+			if (number > 1000)
+			{
+				thousands = (number / 1000) * 10;
+			}
+			int hundreds = (number % 1000) / 100;
+			int sum = thousands + hundreds;
+			Console.WriteLine(sum);
 
-public static class Test
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello");
-        Console.WriteLine("Word");
-        Console.WriteLine("1234");
-    }
-}
+		}
+	}
+
---
+++ lab1-course4/Program2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_course4
+{
+    class Program2
+    {
+
+    }
+}
