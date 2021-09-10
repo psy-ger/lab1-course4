e2f9c31f06c08c0ca3c024c6f0786388ef6e9b4c
PsyGer <gruvoz.1234@gmail.com>
2021-09-10 12:35:38 +0300

    lab1 not full , but working

---
+++ lab1-course4/lab_1_File_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace lab_1_File_2
+{
+    public class Program2
+    {
+        public int sum;
+        public string Raschet()
+        {
+
+            int thousands = 1;
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 1000)
+            {
+                thousands = (number / 1000) * 10;
+            }
+            int hundreds = (number % 1000) / 100;
+            sum = thousands + hundreds;
+            return "";
+        }
+
+
+        public int j;
+        public string MyClass()
+        {
+            j = sum;
+            string str = Convert.ToString(j);
+            return str;
+        }
+
+    }
+}
---
+++ lab1-course4/lab_1_File_2.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+using lab_1_File_2;
+
+namespace lab_1_File_1
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+
+            Program2 blabla = new Program2();
+            Console.Write(blabla.Raschet());
+            Console.Write(blabla.MyClass()); 
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
